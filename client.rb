0debe753ae0624ec017465701b455ad58a3359f1
Chris Wood <chriswood.ca@gmail.com>
2014-02-03 20:52:14 -0800

    Started Curses work and some forking
    ^

--- client.rb
+++ client.rb
@@ -5,10 +5,20 @@
 #
 
 require 'socket'
+require 'curses'
+require 'pry'
 
 #default port
 default_port = 8005
 localhost = "127.0.0.1"
+Curses.noecho
+Curses.init_screen
+
+def report(txt)
+	Curses.setpos(1,0)
+	Curses.addstr(txt)
+	Curses.refresh
+end
 
 if ARGV.empty? || ARGV.count > 3
 	puts "Proper usage: ./client.rb server_addr [server_port] [numClients]"
@@ -28,3 +38,33 @@ elsif ARV.count == 3
 end
 
 ARGV.clear
+
+processes = (1..numClients).map do |p|
+	Process.fork do
+		begin
+			puts "starting process"
+			#s = TCPSocket.open(srv.chomp, port)
+			x = 1
+			loop {
+			#	s.puts "hello world from #{Process.pid}"
+			binding.pry
+				puts x+1
+				report(1)
+			}
+		rescue
+			#socket error
+		ensure
+			#s.close
+		end
+	end
+end
+processes.each {|p| Process.wait p} 
+while cmd = gets.chomp
+	break if cmd.eql? "stop"
+end
+
+
+
+
+Curses.close_screen
+
